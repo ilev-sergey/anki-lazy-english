4462a21fce1232c0185ad70ea348cb2b0670718a
Sergey Ilev <ilev.sv@phystech.edu>
2023-03-05 21:59:21 +0300

    add getModel()

--- anki.py
+++ anki.py
@@ -5,6 +5,9 @@ import sys
 import psutil
 import requests
 
+modelName = 'Lazy English Cards'
+deckName = 'Lazy English'
+
 
 def request(action, **params):
     return {"action": action, "params": params, "version": 6}
@@ -31,4 +34,36 @@ def openAnki():
             os.startfile('C:\\Program Files\\Anki\\anki.exe')
 
 
+def getModel(modelName=modelName):
+    """Create params for createModel function"""
+    folder = 'assets/'
+    os.chdir(folder)
+    with open('styling.css', 'r') as file:
+        css = file.read()
+    with open('back.html', 'r') as file:
+        backHtml = file.read()
+    with open('front.html', 'r') as file:
+        frontHtml = file.read()
+    os.chdir('..')
+    return {
+        'modelName': modelName,
+        'inOrderFields': ['Word', 'Sound', 'Meaning', 'IPA'],
+        'isCloze': False,
+        'css': css,
+        'cardTemplates': [{
+            'Name': modelName,
+            'Front': frontHtml,
+            'Back': backHtml
+        }]
+    }
+
+
+    
+os.chdir('.')
+
 openAnki()
+
+if modelName not in invoke('modelNames'):
+    invoke('createModel', **getModel(modelName=modelName))
+if deckName not in invoke('deckNames'):
+    invoke('createDeck', deck=deckName)
