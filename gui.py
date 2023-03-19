40df3aadd6374dd8546b4867f853ef12563e9f51
Sergey Ilev <ilev.sv@phystech.edu>
2023-03-19 21:13:13 +0300

    add threading for responsive interaction
    Add a worker thread to prevent the GUI freezing when launching and creating cards.

--- gui.py
+++ gui.py
@@ -1,21 +1,40 @@
+import functools
 import json
 import logging
 
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import QObject, Qt, QThread, pyqtSignal
 from PyQt6.QtGui import QKeySequence, QShortcut
 from PyQt6.QtWidgets import (
     QApplication,
     QDialog,
+    QFileDialog,
     QHBoxLayout,
     QLabel,
     QPushButton,
     QTextEdit,
     QVBoxLayout,
-    QFileDialog,
 )
 
 import app
 from constants import *
+
+
+class Worker(QObject):
+    started = pyqtSignal()
+    finished = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self._task = None
+
+    def setTask(self, task):
+        self._task = task
+
+    def run(self):
+        if self._task:
+            self.started.emit()
+            self._task()
+            self.finished.emit()
 
 
 class LazyDialog(QDialog):
@@ -72,7 +91,7 @@ class LazyDialog(QDialog):
     def clearInput(self):
         self.inputField.clear()
         self.setFocusOnInput()
-        logging.info("cleared")
+        logging.info("Cleared")
 
     def getInput(self):
         return self.inputField.toPlainText()
@@ -88,6 +107,12 @@ class LazyDialog(QDialog):
         cursor.setPosition(len(self.inputField.toPlainText()))
         self.inputField.setTextCursor(cursor)
 
+    def disableSubmit(self):
+        self.buttons["Submit"].setDisabled(True)
+
+    def enableSubmit(self):
+        self.buttons["Submit"].setDisabled(False)
+
     class QTextEditLogger(logging.Handler):
         """Logging widget"""
 
@@ -100,60 +125,90 @@ class LazyDialog(QDialog):
             self.widget.setText(msg)
 
 
+def threading(func):
+    def wrapper(self, *args, **kwargs):
+        funcWithArgs = functools.partial(func, self, *args, **kwargs)
+        self.worker.setTask(funcWithArgs)
+        self.thread.start()
+
+    return wrapper
+
+
 class LazyController:
     """Controller class"""
 
     def __init__(self, view, model):
         self._view = view
-        self._model = model
+
+        self._initializeThread()
         self._connectSignalsAndSlots()
+        self._initializeModel()
+
+    def _initializeThread(self):
+        self.thread = QThread()
+        self.worker = Worker()
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
 
     def _connectSignalsAndSlots(self):
-        self._view.buttons["Submit"].clicked.connect(self._createNotes)
-        self._view.buttons["Upload"].clicked.connect(self._uploadFile)
+        self._view.buttons["Submit"].clicked.connect(
+            lambda checked: self.createNotes()  # not to pass checked
+        )
+        self._view.buttons["Upload"].clicked.connect(self.uploadFile)
         self._view.buttons["Clear"].clicked.connect(self._view.clearInput)
 
-        self._view.shortcuts["Ctrl+Return"].activated.connect(self._createNotes)
-        self._view.shortcuts["Ctrl+O"].activated.connect(self._uploadFile)
+        self._view.shortcuts["Ctrl+Return"].activated.connect(self.createNotes)
+        self._view.shortcuts["Ctrl+O"].activated.connect(self.uploadFile)
 
-    def _createNotes(self):
+        self.worker.started.connect(self._view.disableSubmit)
+        self.worker.finished.connect(self._view.enableSubmit)
+
+    @threading
+    def _initializeModel(self):
+        LazyModel._initialize()
+
+    @threading
+    def createNotes(self):
         words = self._view.inputField.toPlainText().split()
-        self._model.createNotes(words)
-        logging.info("cards created")
+        LazyModel._createNotes(words)
 
-    def _uploadFile(self):
+    def uploadFile(self):
         filename = QFileDialog.getOpenFileName()[0]
-        with open(filename) as file:
-            words = file.read()
-        self._view.setInput(words)
+        if filename:
+            with open(filename) as file:
+                words = file.read()
+                self._view.setInput(words)
 
 
 class LazyModel:
     """Model class"""
 
-    def __init__(self):
+    @staticmethod
+    def _initialize():
+        logging.info("Initialization...")
         app.open_anki()
-        self._initialize()
-
-    def _initialize(self):
         if MODEL_NAME not in app.invoke("modelNames"):
             app.invoke("createModel", **app.get_model(model_name=MODEL_NAME))
         if DECK_NAME not in app.invoke("deckNames"):
             app.invoke("createDeck", deck=DECK_NAME)
+        logging.info("Ready to use")
 
-    def createNotes(self, words):
+    @staticmethod
+    def _createNotes(words):
+        logging.info("Creating cards...")
         cache = app.load_cache() if CACHE_ENABLED else {}
         app.invoke("addNotes", notes=app.get_notes(words, cache=cache))
         if CACHE_ENABLED:
             with open(CACHE_PATH, "w", encoding="utf-8") as file:
                 json.dump(cache, file, indent=2)
+        logging.info("Сards created")
 
 
 def main():
     app = QApplication([])
     dialog = LazyDialog()
-    model = LazyModel()
-    controller = LazyController(view=dialog, model=model)
+    controller = LazyController(view=dialog, model=LazyModel)
 
     dialog.show()
     app.exec()
